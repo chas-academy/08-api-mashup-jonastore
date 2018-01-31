ceaa767d0a5ac70c043e0d5d075b7618b1eb678a
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-01-31 19:48:45 +0100

    images div is now reset after each search

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ let flickrParams = "&tags='" + searchTerm + "'&format=json&nojsoncallback=1";
 
 
 $("#searchButton").on("click", function() {
+  $('#images').empty();
   let searchTerm = $("#searchBar").val();
   var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?&tags='" + searchTerm + "'";
   $.getJSON( flickerAPI, {
@@ -20,7 +21,7 @@ $("#searchButton").on("click", function() {
   })
     .done(function( data ) {
       $.each( data.items, function( i, item ) {
-        let searchTerm = null;
+        //let searchTerm = null;
         $( "<img>" ).attr( "src", item.media.m).appendTo( "#images" );
         if ( i === 3 ) {
           return false;
