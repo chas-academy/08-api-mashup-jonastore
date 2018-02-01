529630224ffcae352d449436034a3fff496f0f49
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-02-01 13:08:06 +0100

    added a layout that makes sense

--- src/app.js
+++ src/app.js
@@ -8,46 +8,83 @@ let flickrParams = "&tags='" + searchTerm + "'&format=json&nojsoncallback=1";
 let wordKey = "1efdcf3985947128f17dd03002a9f10e";
 */
 
-function flickr(searchTerm) { //add words funtion here?
-  $('#results').empty();
-  var flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?&tags='" + searchTerm + "'";
-  $.getJSON( flickrUrl, { format: "json" } )
+//flickr api search
+
+/*
+function flickr(searchTerm) {
+    //var flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?&safe_search=&jsoncallback=?&tags='" + searchTerm + "'";
+    let flickrAPI = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=";
+    let flickrKey = "b9ffbd74f90f37df094b59bd2915ff37";
+    let flickrParams = "&tags='" + searchTerm + "'&format=json&nojsoncallback=1";
+    let flickrUrl = flickrAPI + flickrKey + flickrParams;
+
+    $.ajax({
+      url: flickrUrl,
+      type: 'GET',
+      dataType: 'json',
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      success: function(data, status, jqXHR) {
+        console.log(data);
+      } 
+
+    })
     .done(function( data ) {
-      $.each( data.items, function( i, item ) {
-        $("#results").append(String("<a href='" + item.link + "'><img src='" + item.media.m + "'></a>"));
-        if ( i > 3 ) {
-          return false;
-        }
+      console.log("done");
+      
+    })
+    .fail(function( data ) {
+      console.log("fail");
+    })
+};*/
+
+
+
+
+function flickr(searchTerm) {
+  $('#results').empty();
+  var flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?&safe_search=&jsoncallback=?&tags='" + searchTerm + "'";
+  $.getJSON( flickrUrl, { format: "json", async: true } ).done(function( data ) {
+      $.each( data.items, function( i, image ) {
+        $("#results").append(String("<a href='" + image.link + "'><img src='" + image.media.m + "'></a>"));
+        //if ( i > 3 ) {
+        //  return false;
+        //}
       });
     });
 };
 
+//bighugelabs words api search
+
 function words(searchTerm) {
   $('#related').empty();
   var flickrUrl = "http://words.bighugelabs.com/api/2/1efdcf3985947128f17dd03002a9f10e/" + searchTerm + "/json";
-  $.getJSON( flickrUrl, { format: "json" } )
-    .done(function( data ) {
+  $.getJSON( flickrUrl, { format: "json", async: true } ).done(function( data ) {
       $.each( data, function( i, word ) {
-        $.each(word, function(y, words){
-          $("#related").append(String("<button class='singleWord' value=" + words[0] + ">" + words[0] + "</button>").replace(",", " "));
+        $.each(word, function(i, word){
+          $("#related").append(String("<button class='relatedWord' value=" + word[0] + ">" + word[0] + "</button>").replace(",", " "));
+          console.log("data is loaded");
         });
       });
     });
 };
 
-
-$("body").on("click", ".singleWord", function() {
+//related words button
+$("body").on("click", ".relatedWord", function() {
   let searchTerm = this.value;
+  $("#searchBar").val(this.value);
   flickr(searchTerm);
   words(searchTerm);
 });
 
+//search bar button
 $( "#searchButton" ).click(function() {
   let searchTerm = $("#searchBar").val();
   flickr(searchTerm);
   words(searchTerm);
 });
 
+//clear search button
 $( "#clearSearch" ).click(function() {
   $("#related").empty();
   $("#results").empty();
