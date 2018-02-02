4e4caf533977b2cfdd26100bdb939190f9e4951b
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-02-02 11:52:02 +0100

    changed safe search level to 3

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import './app.scss';
 //flickr api search
 function flickr(searchTerm) {
   $('#results').empty();
-  var flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?&safe_search=3&safe_search=&jsoncallback=?&tags='" + searchTerm + "'";
+  var flickrUrl = "http://api.flickr.com/services/feeds/photos_public.gne?&safe_search=3&safe_search=3&jsoncallback=?&tags='" + searchTerm + "'";
   $.getJSON( flickrUrl, { format: "json", async: true } ).then(function( data ) {
       $.each( data.items, function( i, image ) {
         $("#results").append(String("<a href='" + image.link + "'><img src='" + image.media.m + "'></a>"));
@@ -18,9 +18,9 @@ function words(searchTerm) {
   var flickrUrl = "http://words.bighugelabs.com/api/2/1efdcf3985947128f17dd03002a9f10e/" + searchTerm + "/json";
   $.getJSON( flickrUrl, { format: "json", async: true } ).then(function( data ) {
       $.each( data, function( i, word ) {
-        $.each(word, function(i, word){
+        $.each(word, function( i, word ){
           $("#related").append(String("<button class='relatedWord' value=" + word[0] + ">" + word[0] + "</button>").replace(",", " "));
-        });
+        }); 
       });
     });
 };
@@ -46,4 +46,3 @@ $( "#clearSearch" ).click(function() {
   $("#results").empty();
   $("#searchBar").val(null);
 });
-
